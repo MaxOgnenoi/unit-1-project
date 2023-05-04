034ffb7bd41c548d21dfb80efd13e440ae968386
Max Ognenoi <maksymohnenyi>
2023-05-03 20:17:05 -0400

    added randomized enemy pictures

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let myScore = 0
 
 
 const myCircleBtn = document.getElementById('myCircle')
+const enemyCircleBtn = document.getElementById('enemyCircle')
+let allPictures = [
+    "url('images/spok.png')",
+    "url('images/paper.png')",
+    "url('images/scissors.png')",
+    "url('images/rock.png')",
+    "url('images/lizard.png')"
+]
 const spokBtn = document.getElementById('spok')
 const paperBtn = document.getElementById('paper')
 const scissorsBtn = document.getElementById('scissors')
@@ -13,24 +21,44 @@ const lizardBtn = document.getElementById('lizard')
 spokBtn.addEventListener('click', () => {
     myCircleBtn.style.backgroundImage = "url('images/spok.png')"
     myCircleBtn.style.backgroundSize = "contain";
+    let randomNumber = Math.floor(Math.random() * 5)
+    let randomPicture = allPictures[randomNumber]
+    enemyCircleBtn.style.backgroundImage = randomPicture
+    enemyCircleBtn.style.backgroundSize = "contain";
 });
 
 paperBtn.addEventListener('click', () => {
     myCircleBtn.style.backgroundImage = "url('images/paper.png')"
     myCircleBtn.style.backgroundSize = "contain";
+    let randomNumber = Math.floor(Math.random() * 5)
+    let randomPicture = allPictures[randomNumber]
+    enemyCircleBtn.style.backgroundImage = randomPicture
+    enemyCircleBtn.style.backgroundSize = "contain";
 });
 
 scissorsBtn.addEventListener('click', () => {
     myCircleBtn.style.backgroundImage = "url('images/scissors.png')"
     myCircleBtn.style.backgroundSize = "contain";
+    let randomNumber = Math.floor(Math.random() * 5)
+    let randomPicture = allPictures[randomNumber]
+    enemyCircleBtn.style.backgroundImage = randomPicture
+    enemyCircleBtn.style.backgroundSize = "contain";
 });
 
 rockBtn.addEventListener('click', () => {
     myCircleBtn.style.backgroundImage = "url('images/rock.png')"
     myCircleBtn.style.backgroundSize = "contain";
+    let randomNumber = Math.floor(Math.random() * 5)
+    let randomPicture = allPictures[randomNumber]
+    enemyCircleBtn.style.backgroundImage = randomPicture
+    enemyCircleBtn.style.backgroundSize = "contain";
 });
 
 lizardBtn.addEventListener('click', () => {
     myCircleBtn.style.backgroundImage = "url('images/lizard.png')"
     myCircleBtn.style.backgroundSize = "contain";
+    let randomNumber = Math.floor(Math.random() * 5)
+    let randomPicture = allPictures[randomNumber]
+    enemyCircleBtn.style.backgroundImage = randomPicture
+    enemyCircleBtn.style.backgroundSize = "contain";
 });
